60e8bdc05743b9deb395f75b3418f8ce9e1bb9a1
AndreaParrini <andrea.parrini26@gmail.com>
2024-06-15 14:35:12 +0200

    add item og pagination import in section fotos

---
+++ src/components/PaginationItem.vue
@@ -0,0 +1,36 @@
+<script>
+import { store } from '../store';
+export default {
+    name: 'PaginationItem',
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+        goTo(url) {
+            url = store.controlInput(url);
+            console.log(url);
+            store.callApiFotos(url);
+        }
+
+    }
+
+}
+</script>
+<template>
+    <nav aria-label="Page navigation " class="py-4">
+        <ul class="pagination justify-content-end">
+            <li v-for="link in store.fotos.links    " class="page-item"
+                :class="!(link.url) ? 'disabled' : '', link.active ? 'active' : ''">
+                <button class="page-link" :href="link.url" type="button" @click="goTo(link.url)">
+                    <span v-html="link.label"></span>
+                </button>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+
+<style scoped></style>
